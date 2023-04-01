bf69a2c372f2d605a0db9ff58cedb57be15838e0
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-04-02 00:55:15 +0700

    feature(Project): реализован класс Project, содержащий в себе список всех контактов и методы для работы с этим списком

--- src/ContactsApp/ContactsApp.Model/Project.cs
+++ src/ContactsApp/ContactsApp.Model/Project.cs
@@ -6,6 +6,99 @@ using System.Threading.Tasks;
 
 namespace ContactsApp.Model;
 
+/// <summary>
+/// Проект.
+/// </summary>
 internal class Project
 {
+	/// <summary>
+	/// Список всех контактов, созданных в приложении.
+	/// </summary>
+	private List<Contact> _contacts;
+
+	/// <summary>
+	/// Возвращает кол-во контактов в списке.
+	/// </summary>
+	public int ContactsCount
+	{
+		get { return _contacts.Count; }
+	}
+
+	/// <summary>
+	/// Возвращает или задает контакт по индексу.
+	/// </summary>
+	/// <param name="index">Индекс контакта.</param>
+	public Contact this[int index]
+	{
+		get { return _contacts[index]; }
+		set
+		{
+			_contacts[index] = value;
+			SortContactsByFullName();
+		}
+	}
+
+	/// <summary>
+	/// Удаляет контакт из списка.
+	/// </summary>
+	/// <param name="index">Индекс контакта.</param>
+	void RemoveContact(int index)
+	{
+		_contacts.RemoveAt(index);
+	}
+
+	/// <summary>
+	/// Добавляет контакт в список.
+	/// </summary>
+	/// <param name="contact">Контакт, который нужно добавить.</param>
+	void AddContact(Contact contact)
+	{
+		_contacts.Add(contact);
+	}
+
+	/// <summary>
+	/// Сортирует контакты в списке по полному имени.
+	/// </summary>
+	void SortContactsByFullName()
+	{
+		var sortedContacts = _contacts.OrderBy(x => x.FullName);
+		_contacts = sortedContacts.ToList();
+	}
+
+	/// <summary>
+	/// Находит в списке контактов именинников.
+	/// </summary>
+	/// <returns>Список именнинков.</returns>
+	List<Contact> FindBirthdayPeople()
+	{
+		List<Contact> result = new();
+		foreach (Contact contact in _contacts)
+		{
+			if (contact.DateOfBirth == DateTime.Today)
+			{
+				result.Add(contact);
+			}
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// Находит контакты по подстроке.
+	/// </summary>
+	/// <param name="subString">Подстрока, по которой производится поиск.</param>
+	/// <returns>Список контактов, содержащих в себе подстроку.</returns>
+	List<Contact> FindContactsBySubstring(string subString)
+	{
+		List<Contact> result = new();
+		foreach (Contact contact in _contacts)
+		{
+			if (contact.FullName.Contains(subString))
+			{
+				result.Add(contact);
+			}
+		}
+
+		return result;
+	}
 }
