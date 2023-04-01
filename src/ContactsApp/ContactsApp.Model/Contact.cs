2e3fa4d3ec30d7c0cdca10b07fb0097bac06b0cb
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-04-01 23:18:19 +0700

    feature(Contact): написана реализация класса Contact с информацией о контакте

--- src/ContactsApp/ContactsApp.Model/Contact.cs
+++ src/ContactsApp/ContactsApp.Model/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +33,7 @@ internal class Contact
 	private DateTime _dateOfBirth;
 
 	/// <summary>
-	/// ID ВКонтакте контакта
+	/// ID ВКонтакте контакта.
 	/// </summary>
 	private string _vkId;
 
@@ -42,34 +43,28 @@ internal class Contact
 	public string FullName
 	{
 		get { return _fullName; }
-		set 
-		{ 
-			if(value.Length > 100)
+		set
+		{
+			if (value.Length > 100)
 			{
-				throw 
+				throw
 					new ArgumentException("Полное имя не может быть больше 100 символов!");
 			}
-			/*string nameToCheck = value;
-			string[] nameArray = nameToCheck.Split(' ');
-
-			for (int i = 0; i < nameArray.Length; i++)
-			{
-
-				if (nameArray[i][0].ToString().ToUpper() != nameArray[i][0].ToString())
-				{
-
-				}
-			}*/
-			_fullName = value;
+			TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+			string titleCase = textInfo.ToTitleCase(value);
+			_fullName = titleCase;
 		}
 	}
 
+	/// <summary>
+	/// Возвращает или задает E-mail контакта.
+	/// </summary>
 	public string Email
 	{
 		get { return _email; }
 		set
 		{
-			if(value.Length > 100)
+			if (value.Length > 100)
 			{
 				throw
 					new ArgumentException("Email не может быть больше 100 символов!");
@@ -77,31 +72,33 @@ internal class Contact
 		}
 	}
 
+	/// <summary>
+	/// Возвращает или задает телефонный номер контакта.
+	/// </summary>
 	public string PhoneNumber
 	{
 		get { return _phoneNumber; }
-		set
-		{
-			
-			_phoneNumber = value;
-		}
+		set { _phoneNumber = value; }
 	}
 
+	/// <summary>
+	/// Возвращает или задает день рождения контакта.
+	/// </summary>
 	public DateTime DateOfBirth
 	{
 		get { return _dateOfBirth; }
-		set
-		{
-			_dateOfBirth = value;
-		}
+		set { _dateOfBirth = value; }
 	}
 
+	/// <summary>
+	/// Возвращает или задает ID Вконтакте контакта.  
+	/// </summary>
 	public string VKId
 	{
 		get { return _vkId; }
 		set
 		{
-			if(value.Length > 50) 
+			if (value.Length > 50)
 			{
 				throw
 					new ArgumentException("VKId не может быть больше 50 символов!");
