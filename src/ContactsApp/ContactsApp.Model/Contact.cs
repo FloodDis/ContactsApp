748cc95f4c4bfad7d258a46be6e3b66d5be1f654
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-04-15 14:56:27 +0700

    codestyle(Contact): исправлены ошибки

--- src/ContactsApp/ContactsApp.Model/Contact.cs
+++ src/ContactsApp/ContactsApp.Model/Contact.cs
@@ -16,7 +16,23 @@ public class Contact
 
 	/// <summary>
 	/// Паттерн для валидации номера телефона.
-	/// Пример правильного номера телефона: 8(926)123-45-67, +79261234567, 8-926-123-45-67
+	/// Пример правильного номера телефона: 
+	/// +79261234567
+	/// 89261234567
+	/// 79261234567
+	/// +7 926 123 45 67
+	/// 8(926)123-45-67
+	/// 123-45-67
+	/// 9261234567
+	/// 79261234567
+	/// (495)1234567
+	/// (495) 123 45 67
+	/// 89261234567
+	/// 8-926-123-45-67
+	/// 8 927 1234 234
+	/// 8 927 12 12 888
+	/// 8 927 12 555 12
+	/// 8 927 123 8 123
 	/// </summary>
 	private const string _phoneNumberRegex = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$";
 
@@ -50,9 +66,9 @@ public class Contact
 	/// </summary>
 	public string FullName
 	{
-		get 
-		{ 
-			return _fullName; 
+		get
+		{
+			return _fullName;
 		}
 		set
 		{
@@ -79,7 +95,7 @@ public class Contact
 		set
 		{
 			if (value.Length > 100)
-				
+
 			{
 				var message = "Полное имя не может быть больше 100 символов.";
 				throw new ArgumentException(message);
@@ -102,14 +118,14 @@ public class Contact
 		{
 			return _phoneNumber;
 		}
-		set 
-		{ 
-			if(!Regex.IsMatch(value, _phoneNumberRegex))
+		set
+		{
+			if (!Regex.IsMatch(value, _phoneNumberRegex))
 			{
 				var message = "Номер телефона введен неверно.";
 				throw new ArgumentException(message);
 			}
-			_phoneNumber = value; 
+			_phoneNumber = value;
 		}
 	}
 
