4665f736bba3371b8e58ca2b73c609fa77b64ca7
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-04-28 22:18:19 +0700

    codestyle(ContactForm): добавлены комментарии к методам

--- src/ContactsApp/ContactsApp.View/ContactForm.cs
+++ src/ContactsApp/ContactsApp.View/ContactForm.cs
@@ -12,26 +12,52 @@ namespace ContactsApp.View;
 
 public partial class ContactForm : Form
 {
+	/// <summary>
+	/// Конструктор формы.
+	/// </summary>
 	public ContactForm()
 	{
 		InitializeComponent();
 	}
 
+	/// <summary>
+	/// Изменяет цвет картинки для добавления фотографии
+	/// при наведении на нее курсора.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void AddPhotoButton_MouseEnter(object sender, EventArgs e)
 	{
 		AddPhotoButton.Image = Properties.Resources.add_photo_32x32;
 	}
 
+	/// <summary>
+	/// Изменяет цвет картинки для добавления фотографии
+	/// при отведении от нее курсора.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void AddPhotoButton_MouseLeave(object sender, EventArgs e)
 	{
 		AddPhotoButton.Image = Properties.Resources.add_photo_32x32_gray;
 	}
 
+	/// <summary>
+	/// Закрывает форму.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void CancelButton_Click(object sender, EventArgs e)
 	{
 		Close();
 	}
 
+	/// <summary>
+	/// Запрещает вводить в поле для номера телефона все
+	/// символы, кроме цифр и '-', '(', ')', ' ', '+'.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void PhoneNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
 	{
 		if ((e.KeyChar >= '0')
