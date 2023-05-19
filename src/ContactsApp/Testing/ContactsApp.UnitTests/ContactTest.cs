eb53f10971a33ffdeb02d69e9c9620ae53259ca0
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-05-20 01:20:00 +0700

    feature(ContactTest): написаны тесты для свойства FullName

--- src/ContactsApp/Testing/ContactsApp.UnitTests/ContactTest.cs
+++ src/ContactsApp/Testing/ContactsApp.UnitTests/ContactTest.cs
@@ -1,10 +1,49 @@
 namespace ContactsApp.UnitTests;
 
+[TestFixture]
 public class ContactTest
 {
-	[Test]
-	public void Test1()
+	[Test(Description = "Позитивный тест геттера и сеттера FullName")]
+	public void Test_FullName_CorrectValue()
 	{
-		Assert.Pass();
+		//Setup
+		Contact contact = new Contact();
+
+		//Testing
+		var expected = "Сеченов Владислав";
+		contact.FullName = expected;
+		var actual = contact.FullName;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Негативный тест геттера и сеттера FullName (длина имени больше 100)")]
+	public void Test_FullName_FalseValue()
+	{
+		//Setup
+		Contact contact = new Contact();
+
+		//Testing
+		var wrongFullName = "Сеченов ВладиславСеченов ВладиславСеченов ВладиславСеченов ВладиславСеченов ВладиславСеченов ВладиславСеченов ВладиславСеченов Владислав";
+		
+		//Assert
+		Assert.Throws<ArgumentException>(() => contact.FullName = wrongFullName,
+			"Должно возникать исключение, если полное имя длиннее 100 символов");
+	}
+
+	[Test(Description = "Тест геттера и сеттера FullName (каждое слово начинается с заглавной буквы)")]
+	public void Test_FullName_TitleCase()
+	{
+		//Setup
+		Contact contact = new Contact();
+
+		//Testing
+		var expected = "Сеченов Владислав";
+		contact.FullName = "сеченов владислав";
+		var actual = contact.FullName;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
 	}
 }
