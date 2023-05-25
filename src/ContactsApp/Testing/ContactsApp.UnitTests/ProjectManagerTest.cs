989a2cf3b0665def3620684836afad946c9ba620
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-05-26 00:05:45 +0700

    feature(ProjectManagerTest): написаны тесты для класса ProjectManagerTest

---
+++ src/ContactsApp/Testing/ContactsApp.UnitTests/ProjectManagerTest.cs
@@ -0,0 +1,39 @@
+﻿namespace ContactsApp.UnitTests;
+
+[TestFixture]
+internal class ProjectManagerTest
+{
+	[Test(Description = "Тест сохранения и загрузки списка контактов")]
+	public void Test_ProjectManager_SaveLoad()
+	{
+		// Setup
+		var project = new Project();
+		var contactToAdd = ContactFactory.CreateContact();
+
+		// Testing
+		project.AddContact(contactToAdd);
+		project.AddContact(contactToAdd);
+		var expected = project.Contacts;
+		ProjectManager.Save(project);
+		var actual = ProjectManager.Load().Contacts;
+
+		// Assert
+		Assert.AreEqual(expected.ToString(), actual.ToString());
+
+		if (File.Exists(ProjectManager.Path))
+		{
+			File.Delete(ProjectManager.Path);
+		}
+	}
+
+	[Test(Description ="Тест свойства Path")]
+	public void Test_ProjectManager_Path() 
+	{
+		var expected = @"..\Notes.txt";
+
+		ProjectManager.Path = expected;
+		var actual = ProjectManager.Path;
+
+		Assert.AreEqual(expected, actual);
+	}
+}
---
+++ src/ContactsApp/Testing/ContactsApp.UnitTests/ProjectTest.cs
@@ -0,0 +1,136 @@
+﻿namespace ContactsApp.UnitTests;
+
+[TestFixture]
+internal class ProjectTest
+{
+	[Test(Description = "Тест добавления контакта и получения контакта из списка по индексу")]
+	public void Test_Project_IndexAddContact()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var expected = new Contact();
+		project.AddContact(expected);
+		var actual = project[0];
+
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Тест удаления контакта")]
+	public void Test_Project_Delete()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var firstContact = new Contact();
+		var secondContact = new Contact();
+		var thirdContact = new Contact();
+
+		project.AddContact(firstContact);
+		project.AddContact(secondContact);
+		project.AddContact(thirdContact);
+
+		project.RemoveContact(firstContact);
+
+		var expected = secondContact;
+		var actual = project[0];
+
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Тест получения индекса контакта в списке контактов")]
+	public void Test_Project_IndexOf()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var expected = 0;
+		var firstContact = new Contact();
+		project.AddContact(firstContact);
+		var actual = project.IndexOf(firstContact);
+
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Тест получения списка именинников")]
+	public void Test_Project_FindContactsByBirthDay()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var firstContact = new Contact();
+		firstContact.DateOfBirth = new DateTime(2002, DateTime.Today.Month, DateTime.Today.Day);
+		var secondContact = new Contact();
+		secondContact.DateOfBirth = new DateTime(2002, DateTime.Today.Month - 1, DateTime.Today.Day);
+
+		project.AddContact(firstContact);
+		project.AddContact(secondContact);
+
+		var expected = project.FindContactsByBirthDay();
+		var actual = new List<Contact>
+		{
+			firstContact
+		};
+
+		// Assert
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Тест получения списка контактов по подстроке")]
+	public void Test_Project_FindContactsBySubstring()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var firstContact = new Contact();
+		firstContact.FullName = "Сеченов Владислав";
+		var secondContact = new Contact();
+		secondContact.FullName = "Артем Сидоров";
+
+		project.AddContact(firstContact);
+		project.AddContact(secondContact);
+
+		var expected = project.FindContactsBySubstring(project.Contacts,"Артем");
+		var actual = new List<Contact>
+		{
+			secondContact
+		};
+
+		// Assert
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+	[Test(Description = "Тест сортировки списка контактов по полному имени")]
+	public void Test_Project_SortContactsByFullName()
+	{
+		// Setup
+		var project = new Project();
+
+		// Testing
+		var firstContact = new Contact();
+		firstContact.FullName = "Сеченов Владислав";
+		var secondContact = new Contact();
+		secondContact.FullName = "Артем Сидоров";
+
+		project.AddContact(firstContact);
+		project.AddContact(secondContact);
+
+		var expected = project.SortContactsByFullName(project.Contacts);
+		var actual = new List<Contact>
+		{
+			secondContact,
+			firstContact
+		};
+
+		// Assert
+		CollectionAssert.AreEqual(expected, actual);
+	}
+}
